f0396af10f0b2ad802a536b824930f508f54d1e4
nike-getto <savastanisic7@gmail.com>
2023-03-27 18:41:24 +0200

    Changes

--- components/Form.js
+++ components/Form.js
@@ -40,7 +40,7 @@ export default function Form() {
 						htmlFor='startingBlock'
 						className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
 					>
-						Starting Block Number
+						Starting Block Number (0 if not defined)
 					</label>
 					<input
 						className='block w-48 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:italic placeholder:text-slate-400'
